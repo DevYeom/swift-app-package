1c2d27d89ac400604453d1edc2eb2c733d5707c5
SeungYeop Yeom <dev.yeom@gmail.com>
2024-02-10 22:02:16 +0900

    Update the package available platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,13 @@ import PackageDescription
 
 let package = Package(
     name: "AppPackage",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .visionOS(.v1),
+        .watchOS(.v6),
+    ],
     products: [
         .library(
             name: "AppPackage",
